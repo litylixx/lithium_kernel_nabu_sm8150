0f409b2f216d0a1d4e7a2e39e20033fbb6aac81f
litylixx <sergeitananyan@gmail.com>
2025-01-03 01:23:25 +0300

    enable sched_set_*

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1753,6 +1753,9 @@ static inline int task_nice(const struct task_struct *p)
 extern int can_nice(const struct task_struct *p, const int nice);
 extern int task_curr(const struct task_struct *p);
 extern int idle_cpu(int cpu);
+extern int sched_set_fifo(struct task_struct *p);
+extern int sched_set_fifo_low(struct task_struct *p);
+extern int sched_set_normal(struct task_struct *p, int nice);
 extern int sched_setscheduler(struct task_struct *, int, const struct sched_param *);
 extern int sched_setscheduler_nocheck(struct task_struct *, int, const struct sched_param *);
 extern int sched_setattr(struct task_struct *, const struct sched_attr *);
